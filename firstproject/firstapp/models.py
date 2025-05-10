5070face707fbab5b3439955d3f220cdc772eb87
ranad4508 <rand4508@gmail.com>
2025-05-10 08:17:41 +0545

    Django learning project is completed

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -5,3 +5,9 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    reservation_date = models.DateTimeField(auto_now=True)
+    comments = models.CharField(max_length=1000)
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.urls import path
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -11,3 +12,15 @@ def hello_world(request):
 class HelloWorldView(View):
     def get(self, request):
         return HttpResponse("Hello, World! from class-based view")
+    
+
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if  form.is_valid():
+            form.save()
+            return HttpResponse("Reservation created successfully!")
+        
+    return render(request, 'index.html', {'form': form})
